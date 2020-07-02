5e118d40c0d272be6bb7bf7243cca82f22b870a7
Álvaro Patrício <alvaropatricio61@gmail.com>
2020-07-01 23:23:03 -0300

    Toggle na página de produtos
    - Adicionada primeira funcionalidade js (toggle) na página de produtos, faltando adicionar o resto dos produtos e melhorar o design e arranjo dos elementos.

--- js/produtos.js
+++ js/produtos.js
@@ -1,3 +1,10 @@
-const titulos = document.querySelector(".titulo");
-let target = titulos.event.target;
-const categorias = document.querySelector(".categoria");
+const titulos = document.querySelectorAll("div h1");
+const categorias = document.querySelectorAll(".categoria");
+
+for (let i = 0; i < titulos.length; i++) {
+    const listaTitulos = titulos[i];
+    titulos[i].addEventListener('click', function(event) {
+        categorias[i].classList.toggle('hide-element')
+    })       
+}
+
