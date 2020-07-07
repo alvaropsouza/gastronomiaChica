9456c9d4fe6ae4d779805c464629e3229c8b3668
Álvaro Patrício <alvaropatricio61@gmail.com>
2020-07-06 22:56:26 -0300

    Produtos
    Melhorada usabilidade dos botões e criado cardápio

--- js/produtos.js
+++ js/produtos.js
@@ -1,10 +1,31 @@
-const titulos = document.querySelectorAll("div h1");
-const categorias = document.querySelectorAll(".categoria");
+// TOGGLE DOS ITENS DA PÁGINA DE PRODUTOS
+const titulos = document.querySelectorAll(".tipo-produto h1");
+const categorias = document.querySelectorAll(".tipo-produto span");
 
+ 
 for (let i = 0; i < titulos.length; i++) {
-    const listaTitulos = titulos[i];
-    titulos[i].addEventListener('click', function(event) {
-        categorias[i].classList.toggle('hide-element')
-    })       
-}
+    titulos[i].addEventListener('click', () => { categorias[i].classList.toggle('hide-element')
+    categorias[i].classList.add('show-element')} )       
+} 
+
+
+
+// CRIANDO ELEMENTOS COM JS
+// function createAppend(append1, append2, texto) {
+//     let first = document.createElement(append2);
+//     let seccond = document.createElement(append1);
+
+//     seccond.textContent = texto;
+//     first.appendChild(seccond);
+
+//     console.log(first);
+// }
+
+// console.log(createAppend(`div`, `h1`, `texto Teste`));
+
+// !=OPÇÃO PARA ADICIONAR E REMOVER CLASSES DISTINTAS AOS MESMOS ELEMENTOS
+
+
+
+
 
